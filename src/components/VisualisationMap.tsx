9d689beb2d840a2fcc349247712c0ce67b573a50
Bryan Lee <bryan.lee.jh@gmail.com>
2024-11-25 19:45:24 +0800

    fix: fix marker issue

--- src/components/VisualisationMap.tsx
+++ src/components/VisualisationMap.tsx
@@ -3,6 +3,21 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import useTaxiData from "../hooks/useTaxiData";
 
+interface ExtendedIconPrototype extends L.Icon.Default {
+  _getIconUrl?: () => string; // Define the type for the private method
+}
+
+// Fix for Leaflet's default icon paths
+delete (L.Icon.Default.prototype as ExtendedIconPrototype)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
 const VisualisationMap: React.FC = () => {
   const taxiData = useTaxiData();
 
