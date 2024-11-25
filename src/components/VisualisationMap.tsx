7ca87ff69dc21ddfee8f91ad9bc66f06ba12cc3f
Bryan Lee <bryan.lee.jh@gmail.com>
2024-11-25 19:48:10 +0800

    fix: remove marker shadow

--- src/components/VisualisationMap.tsx
+++ src/components/VisualisationMap.tsx
@@ -14,8 +14,7 @@ L.Icon.Default.mergeOptions({
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
   iconUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  shadowUrl: "",
 });
 
 const VisualisationMap: React.FC = () => {
